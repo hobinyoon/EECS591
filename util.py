e0658f0ea89ac3f4a066ed3b724112eff2eb1ae2
Vaspol Ruamviboonsuk <paivaspol@gmail.com>
2015-02-27 14:50:08 -0500

    Added a method for constructing a post request

--- util.py
+++ util.py
@@ -6,3 +6,9 @@ def construct_put_request(url, uuid):
     files = {'file': open(uuid, 'rb')}
     request = requests.put(url, files)
     return request
+
+# Construct a put request which involves a url and a uuid of the file
+def construct_post_request(url, uuid):
+    files = {'file': open(uuid, 'rb')}
+    request = requests.post(url, files)
+    return request
