eb333f18edcddd52aaa8ccb3879fa9a52d4263d6
Vaspol Ruamviboonsuk <paivaspol@gmail.com>
2015-02-27 13:09:00 -0500

    Added DELETE endpoint and endpoints related to writes now updates the metadata

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # Python Library import
+import socket
 import sys
 import os
+import os.path
 import uuid
 
 # Uses Flask for RESTful API
@@ -10,6 +12,7 @@ from flask import Flask, request, send_from_directory
 from werkzeug import secure_filename
 
 # Project imports
+import metadata_manager
 import util
 
 # Constants
@@ -30,46 +33,65 @@ def write_file():
     if file:
         filename = secure_filename(file.filename)
         file_uuid = str(uuid.uuid4())
-        # return str(os.path.join(app.config['UPLOAD_FOLDER'], file_uuid)), 200
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_uuid))
+        metadata = metadata_manager.MetadataManager()
+        metadata.update_file_stored(file_uuid, app.config['HOST'])
         return file_uuid, 201
     return 'Write Failed', 500
 
 # Endpoint for GET method
 @app.route('/read', methods=['GET'])
 def read_file():
-    sys.stdout.flush()
     filename = request.args.get('uuid')
     return send_from_directory(UPLOAD_FOLDER, secure_filename(filename))
 
+# Helper method for sending a file to another server
 def move_file(request):
     file_uuid = request.args.get('uuid')
     destination = request.args.get('destination')
     destination_with_endpoint = destination + '/write'
     write_request = util.construct_put_request(destination_with_endpoint, file_uuid)
+    metadata = metadata_manager.MetadataManager()
+    metadata.update_file_stored(file_uuid, destination)
     return write_request
 
+# Transfers the file. This API call should not be open to all users.
 @app.route('/transfer', methods=['PUT'])
 def transfer():
     write_request = move_file(request)
     if write_request.status_code == 201:
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], file_uuid))
+        metadata.delete_file_stored(request.args.get('uuid'), request.args.get('destination'))
     return write_request
 
+# Replicate the file. This API call should not be open to all users.
 @app.route('/replicate', methods=['PUT'])
 def replicate():
     write_request = move_file(request)
     return write_request
 
+# Deletes the file. This API call should not be open to all users.
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    file_uuid = request.args.get('uuid')
+    file_path = UPLOAD_FOLDER + '/' + file_uuid
+    if (os.path.exists(file_path)):
+        os.remove(file_path)
+        metadata = metadata_manager.MetadataManager()
+        metadata.delete_file_stored(file_uuid, app.config['HOST'])
+        return 'Success', 200
+    return 'File not found', 404
+
+# Returns the log.
 @app.route('/logs', methods=['GET'])
 def logs():
     return 'logs'
 
 if __name__ == '__main__':
-    port = None
+    hostname = '0.0.0.0'
+    port = '5000'
     if len(sys.argv) > 1:
-        port = sys.argv[1]
-    if port == None:
-        app.run(host='0.0.0.0', debug=True)
-    else:
-        app.run(host='0.0.0.0', port=int(port))
+        hostname = sys.argv[1]
+        port = sys.argv[2]
+    app.config['HOST'] = hostname # todo: not sure if this is correct.
+    app.run(host=hostname, port=int(port), debug=True)
