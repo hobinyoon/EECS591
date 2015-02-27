4a6527d633ade5416c1eb0ba19d818ba89aa4f6e
Vaspol Ruamviboonsuk <paivaspol@gmail.com>
2015-02-27 14:30:11 -0500

    Add methods for getting a list of servers in metadata manager

--- metadata_manager.py
+++ metadata_manager.py
@@ -8,9 +8,14 @@ class MetadataManager:
         self.conn = sqlite3.connect('metadata.db')
         self.cursor = self.conn.cursor()
 
-    def test(self):
-        print 'hello!'
-        self.conn.cursor()
+    # Returns the server that stores the file excluding the local machine
+    #
+    # params:
+    #   file_uuid: the file's uuid
+    #   local: the local machine's address
+    def lookup_file(self, file_uuid, local):
+        self.cursor.execute('SELECT server FROM FileMap WHERE uuid=? AND server<>?', (file_uuid, local))
+        return self.cursor.fetchone()
 
     # Adds the file uuid with the server stored into the database
     #
@@ -29,6 +34,14 @@ class MetadataManager:
     def delete_file_stored(self, file_uuid, server_stored):
         self.cursor.execute('DELETE FROM FileMap WHERE uuid=? AND server=?', (file_uuid, server_stored))
         self.conn.commit()
+
+    # Returns a list of the servers that the server knows about excluding itself
+    #
+    # params:
+    #   local: the local address
+    def get_all_server(self, local):
+        self.cursor.execute('SELECT * FROM Server WHERE server<>?', (local))
+        return self.cursor.fetchall()
 
     # Adds the server into the metadata database
     #
