5e4d5f8bd1328cdf3a451f90fd7354bfd78ed78a
Chris Baik <chrisjbaik@gmail.com>
2015-03-17 16:47:56 -0400

    Fix function overload issue in ip_location_cache

--- cache/ip_location_cache.py
+++ cache/ip_location_cache.py
@@ -1,29 +1,42 @@
 # A cache for ip to location mapping
+import os
+import requests
 import sqlite3
+
+# Config
+CACHE_INITIALIZATION = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cache.sql')
 
 class ip_location_cache:
 
     def __init__(self):
         self.conn = sqlite3.connect('ip_location_cache.db')
+        with open(CACHE_INITIALIZATION, 'rb') as initialization_file:
+            self.conn.executescript(initialization_file.read())
         self.cursor = self.conn.cursor()
 
     # Add an entry to the cache based on the ip address.
-    def add_entry(self, ip):
+    def find_and_add_entry(self, ip):
         print 'Retrieving data for ' + ip + '...'
         r = requests.get('http://ipinfo.io/' + ip + '/json')
         try:
             ip_info = r.json()
+            if 'loc' not in ip_info:
+                raise ValueError
             loc = ip_info['loc'].split(',')
-            print 'Data for ' + client_ip[0] + ' found. lat: ' + loc[0] + ', lng: ' + loc[1]
-            add_entry(ip_info['ip'], loc[0], loc[1], ip_info['city'], ip_info['region'], ip_info['country'])
+            print 'Data for ' + ip[0] + ' found. lat: ' + loc[0] + ', lng: ' + loc[1]
+
+            loc[0] = float(loc[0])
+            loc[1] = float(loc[1])
+
+            self.add_entry_to_cache(ip_info['ip'], loc[0], loc[1], ip_info['city'], ip_info['region'], ip_info['country'])
             print 'Inserted into database.'
             return (loc[0], loc[1])
         except ValueError:
-            print 'Error retrieving data for ' + client_ip
+            print 'Error retrieving data for ' + ip
             pass
 
     # Add an entry to the cache.
-    def add_entry(self, ip, lat, lon, city, region, country):
+    def add_entry_to_cache(self, ip, lat, lon, city, region, country):
         self.cursor.execute('INSERT INTO IpLocationMap VALUES (?, ?, ?, ?, ?, ?)', (ip, lat, lon, city, region, country))
         self.conn.commit()
 
@@ -32,6 +45,6 @@ class ip_location_cache:
         self.cursor.execute('SELECT lat, long FROM IpLocationMap WHERE ip=?', (ip,))
         result = self.cursor.fetchone()
         if result is None:
-            return add_entry(ip)
+            return self.find_and_add_entry(ip)
         else:
             return result
