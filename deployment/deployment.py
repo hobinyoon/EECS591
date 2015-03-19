170b61e8cdbcf962e51567cfa012e160a62a3561
Vaspol Ruamviboonsuk <paivaspol@gmail.com>
2015-03-19 09:58:41 -0400

    Updated the deployment script

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -14,9 +14,9 @@ def execute_ssh_command(ssh_client, command):
     print_command(command)
     stdin, stdout, stderr = ssh_client.exec_command(command)
     # for line in stdout.readlines():
-    #    sys.stdout.write(line)
+    #     sys.stdout.write(line)
     # for line in stderr.readlines():
-    #    sys.stdout.write(line)
+    #     sys.stdout.write(line)
     return
 
 CONFIG_FILE = 'deployment.cnf'
@@ -50,6 +50,10 @@ for section in parser.sections():
     username = None
     password = None
     private_key_file = None
+    processes = 1
+    debug = False
+    clear_metadata = False
+    use_distributed_replication = False
 
     if parser.has_option(section, 'username'):
         username = parser.get(section, 'username')
@@ -58,6 +62,14 @@ for section in parser.sections():
     if parser.has_option(section, 'key_filename'):
         private_key_filename = parser.get(section, 'key_filename')
         private_key_file = paramiko.RSAKey.from_private_key_file(private_key_filename)
+    if parser.has_option(section, 'processes'):
+        processes = parser.get(section, 'processes')
+    if parser.has_option(section, 'debug'):
+        debug = bool(parser.get(section, 'debug'))
+    if parser.has_option(section, 'clear_metadata'):
+        clear_metadata = bool(parser.get(section, 'clear_metadata'))
+    if parser.has_option(section, 'use_distributed_replication'):
+        use_distributed_replication = bool(parser.get(section, 'use_distributed_replication'))
 
     base_directory = parser.get(section, 'directory') # Base directory must exists on the target machine
     application_directory = base_directory  + '/' + PROJECT_NAME
@@ -86,7 +98,15 @@ for section in parser.sections():
     # Fifth, run the server
     for file in RUN_FILES:
         file_path = application_directory + '/' + section + '/' + file
-        run_command = 'nohup python ' + file + ' ' + SERVER_LIST_FILE + ' --host ' + host + ' --port ' + deployment_port + ' &'
+        prefix = 'nohup python ' + file + ' ' + SERVER_LIST_FILE + ' --host ' + host + ' --port ' + deployment_port + ' --processes ' + str(processes)
+        if debug:
+            prefix = prefix + ' --with-debug'
+        if clear_metadata:
+            prefix = prefix + ' --clear-metadata'
+        if use_distributed_replication:
+            prefix = prefix + ' --use-dist-replication'
+        suffix = ' &'
+        run_command = prefix + suffix
         execute_ssh_command(ssh, cd_command + run_command)
 
     # Finally, close the connection
