6c2a78fd916a78bcc3a8530548abe17a884d66ca
Chris Baik <chrisjbaik@gmail.com>
2015-03-20 15:09:22 -0400

    Remove volley.sql, unused

--- volley/volley.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS Client(ip text,
-                                  lat real, lng real,
-                                  city text, region text, country text,
-                                  PRIMARY KEY (ip));
