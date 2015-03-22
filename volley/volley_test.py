072912d908f8fedff5a2643c9793ea8f18da500d
Chris Baik <chrisjbaik@gmail.com>
2015-03-22 19:23:19 -0400

    delete useless volley_test (run volley.py instead)

--- volley/volley_test.py
+++
@@ -1,8 +0,0 @@
-import volley
-
-volley = volley.Volley()
-
-# volley.map_ip_to_locations()
-placements_by_server = volley.execute()
-for server, placements in placements_by_server.iteritems():
-  print server + ": " + str(len(placements))
